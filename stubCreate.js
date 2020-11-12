440dd27d7bc3d71846ab47b0d9e10cae6d6d15f4
Sungho <sungho98@utexas.edu>
2020-11-11 22:28:18 -0600

    account page

---
+++ stubCreate.js
@@ -0,0 +1,32 @@
+function verify(event){
+	username=document.getElementById("username").value
+	email=document.getElementById("email").value
+	password1=document.getElementById("password").value
+	repeat=document.getElementById("retype").value
+	
+	var flag= false;
+	var user=/^[a-zA-Z0-9]{1,10}$/
+	var pass=/^(?=.*\d)(?=.*[a-z])[a-zA-Z0-9]{6,10}$/
+	var emal=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+	if (user.test(username)&& pass.test(password1) && password1==repeat && emal.test(email)){
+		alert("User validated")
+		return
+	}
+	else if (user.test(username)== false){
+		alert("Invalid username")
+		return
+	}
+	else if (pass.test(password1)== false) {
+		console.log('huh')
+		alert("Invalid password")
+		return
+	}
+	else if (emal.test(email)== false) {
+		alert("Invalid email")
+		return
+	}
+	else if (password1!=repeat){
+		alert("password does not match")
+		return
+	}
+}
